b40792a5ba567331c7d988615c81ac24d7dc80f4
Ryo Katsuno <62652429+Katsuno0814@users.noreply.github.com>
2024-11-21 18:00:32 +0900

    コメント機能のAPI化

--- src/main/java/in/tech_camp/pictweet/controller/CommentController.java
+++ src/main/java/in/tech_camp/pictweet/controller/CommentController.java
@@ -1,13 +1,13 @@
 package in.tech_camp.pictweet.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import in.tech_camp.pictweet.costom_user.CustomUserDetail;
 import in.tech_camp.pictweet.entity.CommentEntity;
@@ -16,10 +16,10 @@ import in.tech_camp.pictweet.form.CommentForm;
 import in.tech_camp.pictweet.repository.CommentRepository;
 import in.tech_camp.pictweet.repository.TweetRepository;
 import in.tech_camp.pictweet.repository.UserRepository;
-import in.tech_camp.pictweet.validation.ValidationOrder;
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
+@RequestMapping("/api/tweets")
 @AllArgsConstructor
 public class CommentController {
 
@@ -29,36 +29,32 @@ public class CommentController {
 
   private final TweetRepository tweetRepository;
 
-  @PostMapping("/tweets/{tweetId}/comment")
-  public String createComment(@PathVariable("tweetId") Integer tweetId, 
-                            @ModelAttribute("commentForm") @Validated(ValidationOrder.class) CommentForm commentForm,
+  @PostMapping("/{tweetId}/comment")
+  public ResponseEntity<CommentEntity> createComment(@PathVariable("tweetId") Integer tweetId,
+                            @RequestBody CommentForm commentForm,
                             BindingResult result,
-                            @AuthenticationPrincipal CustomUserDetail currentUser, Model model) {
+                            @AuthenticationPrincipal CustomUserDetail currentUser) {
 
-    TweetEntity tweet = tweetRepository.findById(tweetId);
+    TweetEntity tweet = tweetRepository.findTweetById(tweetId);
 
-    if (result.hasErrors()) {
-        model.addAttribute("errorMessages", result.getAllErrors());
-        model.addAttribute("tweet", tweet);
-        model.addAttribute("commentForm", commentForm);
-        return "tweets/detail";
-    }
+    // if (result.hasErrors()) {
+    //   return ResponseEntity.badRequest().body(Collections.singletonMap("errorMessages", result.getAllErrors()));
+    // }
 
     CommentEntity comment = new CommentEntity();
     comment.setText(commentForm.getText());
     comment.setTweet(tweet);
-    comment.setUser(userRepository.findById(currentUser.getId()));
+    comment.setUser(userRepository.findUserById(currentUser.getId()));
 
     try {
       commentRepository.insert(comment);
     } catch (Exception e) {
-      model.addAttribute("tweet", tweet);
-      model.addAttribute("commentForm", commentForm);
       System.out.println("エラー：" + e);
-      return "tweets/detail";
+      // return ResponseEntity.badRequest().body(Collections.singletonMap("errorMessage", "不正な入力です"));
+
     }
 
-    return "redirect:/tweets/" + tweetId;
+    return ResponseEntity.ok(comment);
+
   }
 }
-
--- src/main/java/in/tech_camp/pictweet/repository/CommentRepository.java
+++ src/main/java/in/tech_camp/pictweet/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.One;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
@@ -16,21 +17,22 @@ public interface CommentRepository {
     @Select("SELECT c.*, u.id AS user_id, u.nickname AS user_nickname FROM comments c JOIN users u ON c.user_id = u.id WHERE c.tweet_id = #{tweetId}")
     @Results(value = {
       @Result(property = "user.id", column = "user_id"),
-      @Result(property = "user.nickname", column = "user_nickname"),
-      @Result(property = "tweet", column = "tweet_id", 
-              one = @One(select = "in.tech_camp.pictweet.repository.TweetRepository.findById"))
+      @Result(property = "user.nickname", column = "user_nickname")
+//       @Result(property = "tweet", column = "tweet_id",
+//               one = @One(select = "in.tech_camp.pictweet.repository.TweetRepository.findById"))
     })
     List<CommentEntity> findByTweetId(Integer tweetId);
 
     @Select("SELECT * FROM comments WHERE user_id = #{userId}")
     @Results(value = {
-      @Result(property = "user", column = "user_id", 
+      @Result(property = "user", column = "user_id",
               one = @One(select = "in.tech_camp.pictweet.repository.UserRepository.findById")),
-      @Result(property = "tweet", column = "tweet_id", 
+      @Result(property = "tweet", column = "tweet_id",
               one = @One(select = "in.tech_camp.pictweet.repository.TweetRepository.findById"))
     })
     List<CommentEntity> findByUserId(Integer userId);
 
-    @Insert("INSERT INTO comments (text, user_id, tweet_id) VALUES (#{text}, #{user.id}, #{tweet.id})")
-    void insert(CommentEntity comment);
+   @Insert("INSERT INTO comments (text, user_id, tweet_id) VALUES (#{text}, #{user.id}, #{tweet.id}) RETURNING id")
+   @Options(useGeneratedKeys = true, keyProperty = "id")
+   void insert(CommentEntity comment);
 }
